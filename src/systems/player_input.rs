babb510ffb2ecacff15063c2950df4f0f59696a1
Vincent Alix-Joly <vincent.alix-joly.1@etsmtl.net>
2022-12-26 16:05:33 -0500

    message-based movement

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -2,15 +2,16 @@ use crate::prelude::*;
 use VirtualKeyCode::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
-    #[resource] turn_state: &mut TurnState
+    #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players
+        = <(Entity, &Point)>::query().filter(component::<Player>());
     if let Some(key) = key {
         let delta = match key {
             Left => Point::new(-1, 0),
@@ -19,17 +20,13 @@ pub fn player_input(
             Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
-        if delta.x != 0 || delta.y != 0 {
-            let mut players
-                = <&mut Point>::query().filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let dest = *pos+delta;
-                if map.can_enter_tile(dest) {
-                    *pos = dest;
-                    camera.on_player_move(dest);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            });
-        }
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push(((), WantsToMove {
+                entity: *entity,
+                destination,
+            }));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
