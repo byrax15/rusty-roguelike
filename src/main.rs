a158c7ce559a35fda2c3065dd61a12f2ac31aea2
Vincent Alix-Joly <vincent.alix-joly.1@etsmtl.net>
2022-12-27 17:13:22 -0500

    combat system

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ impl State {
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
-        resources.insert(TurnState::AwaitingInput);
+        resources.insert(AwaitingInput);
 
         Self {
             ecs,
